37d4b31500e9d1f1fc58ba9ed80a4c038489e295
lowerc4s3 <kkirill03@gmail.com>
2025-08-03 16:06:36 +0300

    feat(app): update errors

--- app/errors.go
+++ app/errors.go
@@ -9,6 +9,8 @@ import (
 var (
 	ErrMalformedRequest = echo.NewHTTPError(http.StatusBadRequest, "malformed body")
 	ErrMalformedID      = echo.NewHTTPError(http.StatusBadRequest, "malformed ID")
+	ErrDownload         = echo.NewHTTPError(http.StatusBadGateway, "all requested resources returned error")
+	ErrForbiddenMime    = echo.NewHTTPError(http.StatusBadRequest, "all files had forbidden type")
 	ErrTaskNotFound     = echo.NewHTTPError(http.StatusNotFound, "no task found with provided ID")
 	ErrTooManyTasks     = echo.NewHTTPError(http.StatusServiceUnavailable, "tasks' max amount achieved")
 )
